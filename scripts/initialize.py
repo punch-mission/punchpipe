70326678275d94c26947ae6ad43bfd9985397139
jmbhughes <hughes.jmb@gmail.com>
2022-03-09 14:14:11 -0700

    experiment with active test

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -1,33 +1,34 @@
+import punchpipe
 from punchpipe.infrastructure.controlsegment import ControlSegment, ControlSegmentConfiguration
 from punchpipe.infrastructure.flows import LauncherFlowBuilder, SchedulerFlowBuilder
 from punchpipe.infrastructure.tasks.scheduler import CheckForInputs
 from credentials import db_cred
+from prefect.storage import Local
+
+if __name__ == "__main__":
+    project_name = "punchpipe"
+    process_flows = []
 
 
-project_name = "punchpipe"
-process_flows = []
+    scheduler_flows = []
+    # class Level1InputsCheck(CheckForInputs):
+    #     def run(self):
+    #         pass
+    #
+    #
+    # level1_inputs_check = Level1InputsCheck()
+    # level1_schedule_flow = SchedulerFlowBuilder(db_cred, 1, 1, level1_inputs_check).build()
+
+    launcher_flow = LauncherFlowBuilder(db_cred, 1).build()
+    launcher_flow.storage = Local()
 
 
-scheduler_flows = []
-
-
-class Level1InputsCheck(CheckForInputs):
-    def run(self):
-        pass
-
-
-level1_inputs_check = Level1InputsCheck()
-level1_schedule_task = SchedulerFlowBuilder(db_cred, 1, 1, level1_inputs_check)
-
-launcher_flow = LauncherFlowBuilder(db_cred, 1).build()
-
-
-control_configuration = ControlSegmentConfiguration()
-punchpipe_control_segment = ControlSegment(project_name,
-                                           process_flows,
-                                           scheduler_flows,
-                                           launcher_flow,
-                                           control_configuration,
-                                           db_cred)
-punchpipe_control_segment.register_with_prefect()
+    control_configuration = ControlSegmentConfiguration()
+    punchpipe_control_segment = ControlSegment(project_name,
+                                               process_flows,
+                                               scheduler_flows,
+                                               launcher_flow,
+                                               control_configuration,
+                                               db_cred)
+    punchpipe_control_segment.register_with_prefect()
 
