e02260fc2f34d9a7c9d6a70b09a582a617861be7
jmbhughes <hughes.jmb@gmail.com>
2022-03-30 08:09:26 -0600

    created level 1 placeholders

---
+++ punchpipe/level1/stray_light.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from punchpipe.infrastructure.tasks.core import ScienceFunction
+from punchpipe.infrastructure.data import PUNCHData
+from punchpipe.infrastructure.tasks.core import CalibrationConfiguration
+
+
+class StrayLightRemovalFunction(ScienceFunction):
+
+    def process(self, data_object: PUNCHData, configuration: Optional[CalibrationConfiguration] = None) -> PUNCHData:
+        return data_object  # TODO : actually do stray light removal!
