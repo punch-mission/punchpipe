79a897d3ff7f8557ec9c354903a9af8b2162dcfb
Chris Lowder <lowder@boulder.swri.edu>
2022-04-08 16:48:38 -0600

    Updated PUNCHData object and planned tests

--- punchpipe/infrastructure/data.py
+++ punchpipe/infrastructure/data.py
@@ -1,16 +1,24 @@
 from __future__ import annotations
-from ndcube import NDCube
-from ndcube.visualization import BasePlotter, PlotterDescriptor
-from astropy.io import fits
-from astropy.wcs import WCS
-import astropy.units as u
-from typing import Union, Optional
-import numpy as np
+
+from collections import namedtuple
 from datetime import datetime
+from typing import Union, Optional, List, Dict
+
+import astropy.units as u
+import matplotlib
+import numpy as np
+from astropy.io import fits
+from astropy.nddata import StdDevUncertainty
+from astropy.wcs import WCS
 from dateutil.parser import parse as parse_datetime
+from ndcube import NDCube
+from ndcube.visualization import BasePlotter
+
 
 class PUNCHCalibration:
     pass
+
+# TODO - pull into develop branch once done
 
 
 class PUNCHData:
@@ -56,82 +64,228 @@ class PUNCHData:
         elif data_obj is None:
             self.cubes = dict()
         else:
-            print("Please specify either an NDCube object, or a dictionary of NDCube objects")
+            raise Exception("Please specify either an NDCube object, or a dictionary of NDCube objects")
 
     @classmethod
     def from_fits(cls, inputs: Union[str, List[str], Dict[str, str]]) -> PUNCHData:
         """
-        tries to infer from filename for str and List[str] what the keywords are. If it can't infer... Exception!
-        which tells them to do it manually For the dict it uses the manually indicated keywords
+        Populates a PUNCHData object from specified FITS files.
+        Specify a filename string, a list of filename strings, or a dictionary of keys and filenames
+
         Parameters
         ----------
         inputs
+            input from which to generate a PUNCHData object
+            (filename string, a list of filename strings, or a dictionary of keys and filenames)
+
+        Returns
+        -------
+        PUNCHData object
+
+        """
+
+        if type(inputs) is str:
+            files = {"default": inputs}
+
+        elif type(inputs) is list:
+            files = {}
+            for file in inputs:
+                if type(inputs[file]) is str:
+                    files[file] = file
+                else:
+                    raise Exception("PUNCHData objects are generated with a list of filename strings.")
+
+        elif type(inputs) is dict:
+            files = {}
+            for key in inputs:
+                if type(inputs[key]) is str:
+                    files[key] = inputs[key]
+                else:
+                    raise Exception("PUNCHData objects are generated with a dictionary of keys and string filenames.")
+
+        else:
+            raise Exception("PUNCHData objects are generated with a filename string, a list of filename strings, "
+                            "or a dictionary of keys and filenames")
+
+        data_obj = {}
+
+        for key in files:
+            with fits.open(files[key]) as hdul:
+                data = hdul[0].data
+                meta = hdul[0].header
+                wcs = WCS(hdul[0].header)
+                uncertainty = StdDevUncertainty(hdul[1].data)
+                unit = u.ct  # counts
+                ndcube_obj = NDCube(data, wcs=wcs, uncertainty=uncertainty, meta=meta, unit=unit)
+                data_obj[key] = ndcube_obj
+
+        return cls(data_obj)
+
+    def weight(self, kind: str = "default") -> np.ndarray:
+        """
+        Generate a corresponding weight map from the uncertainty array
+
+        Parameters
+        ----------
+        kind
+            specified element of the PUNCHData object to generate weights
+
+        Returns
+        -------
+        weight
+            weight map computed from uncertainty array
+
+        """
+
+        return 1./self.cubes[kind].uncertainty.array
+
+    def __contains__(self, kind) -> bool:
+        return kind in self.cubes
+
+    def __getitem__(self, kind) -> NDCube:
+        return self.cubes[kind]
+
+    def __setitem__(self, kind, data) -> None:
+        if type(data) is NDCube:
+            self.cubes[kind] = data
+        else:
+            raise Exception("PUNCHData entries must contain NDCube objects.")
+
+    def __delitem__(self, kind) -> None:
+        del self.cubes[kind]
+
+    def clear(self) -> None:
+        """remove all NDCubes"""
+        self.cubes.clear()
+
+    def update(self, other: PUNCHData) -> None:
+        """merge two PUNCHData objects"""
+        self.cubes.update(other)
+
+    def generate_filename(self, kind: str = "default") -> str:
+        """
+        Dynamically generate a filename for the given data product, using the format 'Ln_ttO_yyyymmddhhmmss'
+        Parameters
+        ----------
+        kind
+            specified element of the PUNCHData object to write to file
+
+        Returns
+        -------
+        filename
+            output filename string
+
+        """
+        # TODO - Lookup table for type_codes / matching with metadata (or just specify as FITS keyword?)
+        observatory = self.cubes[kind].meta['OBSRVTRY']
+        file_level = self.cubes[kind].meta['lvl_num']
+        type_code = self.cubes[kind].meta['type_code']
+        date_obs = self.cubes[kind].meta['DATE-OBS']
+        time_obs = self.cubes[kind].meta['TIME-OBS']
+
+        date_string = date_obs[0:4] + date_obs[5:7] + date_obs[8:10]
+        time_string = time_obs[0:2] + time_obs[3:5] + time_obs[6:8]
+        filename = 'L' + file_level + '_' + type_code + observatory + '_' + date_string + time_string
+
+        return filename
+
+    def write(self, kind: str = "default", filename: Optional[str] = None) -> Dict:
+        """
+        Write PUNCHData elements to FITS files
+
+        Parameters
+        ----------
+        kind
+            specified element of the PUNCHData object to write to file
+        filename
+            output filename (will override default naming scheme)
 
         Returns
         -------
 
         """
-        with fits.open(filename) as hdul:
-            data = hdul[0].data
-            meta = hdul[0].header
-            wcs = WCS(hdul[0].header)
-            uncertainty = hdul[1].data
-            mask = hdul[2].data
-            unit = u.ct  # counts
-            ndcube_obj = NDCube(data, wcs=wcs, uncertainty=uncertainty, mask=mask, meta=meta, unit=unit)
-            data_obj = {"wfi-starfield": ndcube_obj}
-        return cls(data_obj)
 
-    # Implement some other functions from dictionary
+        # TODO - Note the astropy.fits.Card verification method for headers
 
-    def __getitem__(self, item) -> None:
-        pass
+        # Generate a standard filename if not overridden
+        if filename is None:
+            filename = self.generate_filename(kind) + '.fits'
 
-    def __setitem__(self, key, value) -> None:
-        pass
-
-    def __delitem__(self, key) -> None:
-        pass
-
-    def clear(self) -> None:
-        """remove all NDCubes"""
-        pass
-
-    def update(self, other: PUNCHData) -> None:
-        """merge two PUNCHData objects"""
-        pass
-
-    def __contains__(self, item):
-        pass
-
-    def merge(self):
-        pass
-
-    def purge(self):
-        # Just call the built in dictionary delitem function?
-        pass
-
-    def write(self, filename: str, kind: str = "default") -> None:
-        """Write to FITS file"""
+        # Populate elements to write to file
         data = self.cubes[kind].data
         uncert = self.cubes[kind].uncertainty
-        mask = self.cubes[kind].mask
         meta = self.cubes[kind].meta
         wcs = self.cubes[kind].wcs
 
         hdu_data = fits.PrimaryHDU()
         hdu_data.data = data
         hdu_data.header = meta
+        # Make this something quick until full header writing is enabled
 
         hdu_uncert = fits.ImageHDU()
-        hdu_uncert.data = uncert
+        hdu_uncert.data = uncert.array
 
-        hdu_mask = fits.ImageHDU()
-        hdu_mask.data = mask
+        hdul = fits.HDUList([hdu_data, hdu_uncert])
 
-        hdul = fits.HDUList([hdu_data, hdu_uncert, hdu_mask])
-
+        # Write to FITS
         hdul.writeto(filename)
+
+        # TODO - Return dictionary of specified metadata items below (and format these)
+        # CREATE TABLE files (
+        # file_ id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+        # level INT NOT NULL,
+        # file_type CHAR(2) NOT NULL,
+        # observatory CHAR(1) NOT NULL,
+        # file_version INT NOT NULL,
+        # software_version INT NOT NULL,
+        # date_acquired DATETIME NOT NULL,
+        # date_obs DATETIME NOT NULL,
+        # date_end DATETIME NOT NULL,
+        # polarization CHAR(2),
+        # state VARCHAR (64) NOT NULL,
+        # processing_fLow CHAR (44) NOT NULL,
+        # file_name char (29) GENERATED ALWAYS AS
+        # (concat("L", level ," ", file_type, observatory,
+        # DATE_FORMAT (date_acquired, 'SY%m%d%H%1%s),
+        # , 'V', file_version,
+        # ". fits' )),
+        # PRIMARY KEY ( file_id ),
+        # FOREIGN KEY ( processing_flow )
+        # REFERENCES fLows (fLow_id)
+
+        update_table = {}
+        return update_table
+
+    def write_image(self, kind: str = "default", filename: Optional[str] = None) -> None:
+        """
+        Write an 8-bit scaled version of the specified data array to a PNG file
+
+        Parameters
+        ----------
+        kind
+            specified element of the PUNCHData object to write to file
+            should be two-dimensional array
+        filename
+            output filename (will override default naming scheme)
+
+        Returns
+        -------
+
+        """
+
+        if self[kind].data.ndim != 2:
+            raise Exception("Specified output data should have two-dimensions.")
+
+        # Generate a standard filename if not overridden
+        if filename is None:
+            filename = self.generate_filename(kind) + '.png'
+
+        # Scale data array to 8-bit values
+        output_data = np.fix(np.interp(self[kind].data, (self[kind].data.min(), self[kind].data.max()), (0, 2**8 - 1)))
+        output_data = np.int(output_data)
+
+        # Write image to file
+        matplotlib.image.saveim(filename, output_data)
 
     def plot(self, kind: str = "default") -> None:
         """Generate relevant plots to display or file"""
@@ -161,6 +315,7 @@ class PUNCHData:
     def date_obs(self, kind: str = "default") -> datetime:
         return parse_datetime(self.cubes[kind].meta.get("date-obs"))
 
+
 class HeaderTemplate:
     """
 
@@ -168,15 +323,37 @@ class HeaderTemplate:
 
     pass
 
+
+HistoryEntry = namedtuple("HistoryEntry", "datetime, source, comment")
+
+
 class History:
-    """
-    
-    """
-    pass
+    def __init__(self):
+        self._entries: List[HistoryEntry] = []
+
+    def add_entry(self, entry: HistoryEntry) -> None:
+        self._entries.append(entry)
+
+    def clear(self) -> None:
+        self._entries = []
+
+    def __getitem__(self, index: int) -> HistoryEntry:
+        return self._entries[index]
+
+    def most_recent(self) -> HistoryEntry:
+        return self._entries[-1]
+
+    def convert_to_fits_cards(self) -> None:
+        pass
+
+    def __len__(self):
+        return len(self._entries)
+
 
 class PUNCHPlotter(BasePlotter):
     """
     Custom PUNCHPlotter class
+    (useful for bespoke colormaps, or special handling of weights / masks)
     """
     def plot(self):
         pass
--- punchpipe/infrastructure/tests/test_data.py
+++ punchpipe/infrastructure/tests/test_data.py
@@ -4,7 +4,8 @@ from punchpipe.infrastructure.data import PUNCHData
 
 
 TESTDATA_DIR = os.path.dirname(__file__)
-SAMPLE_FITS_PATH = os.path.join(TESTDATA_DIR, "L0_CL1_20211111070246.fits")
+SAMPLE_FITS_PATH = os.path.join(TESTDATA_DIR, "L0_CL1_20211111070246_PUNCHData.fits")
+SAMPLE_WRITE_PATH = os.path.join(TESTDATA_DIR, "write_test.fits")
 
 
 @fixture
@@ -12,6 +13,71 @@ def sample_data():
     return PUNCHData.from_fits(SAMPLE_FITS_PATH)
 
 
+@fixture
+def write_data():
+    return PUNCHData.write(SAMPLE_WRITE_PATH)
+
+
 def test_sample_data_creation(sample_data):
     assert isinstance(sample_data, PUNCHData)
 
+
+# Tests of PUNCHData object generation
+
+
+def test_generate_empty():
+    pass
+
+
+def test_generate_from_filename():
+    pass
+
+
+def test_generate_from_filenamelist():
+    pass
+
+
+def test_generate_from_filenamedict():
+    pass
+
+
+def test_generate_from_ndcube():
+    pass
+
+
+def test_generate_from_ndcubedict():
+    pass
+
+
+def test_write_data():
+    pass
+
+
+# Tests of PUNCHData dictionary manipulation
+
+
+def test_cubes_delete():
+    pass
+
+
+def test_cubes_clear():
+    pass
+
+
+def test_cubes_update():
+    pass
+
+
+# Tests of PUNCHData object writing
+
+
+def test_gen_filename():
+    pass
+
+
+def test_write():
+    pass
+
+
+def test_write_png():
+    pass
