729149ce51dcd1abcbcdb7affc5b9d00bda5cceb
jmbhughes <hughes.jmb@gmail.com>
2022-04-29 11:02:44 -0600

    moved level1 inputs check to the level1 subpackage

--- punchpipe/level1/tasks.py
+++ punchpipe/level1/tasks.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
+from datetime import datetime
+import json
+import random
 from prefect import Parameter
+from prefect.tasks.mysql import MySQLFetch, MySQLExecute
 from punchpipe.infrastructure.tasks.core import ScienceTask, IngestTask, OutputTask
-from punchpipe.infrastructure.flows import SchedulerFlowBuilder
+from punchpipe.infrastructure.tasks.scheduler import CheckForInputs
+from punchpipe.infrastructure.data import PUNCHData
+from punchpipe.infrastructure.db import FlowEntry, FileEntry
 from punchpipe.level1.destreak import DestreakFunction
 from punchpipe.level1.alignment import AlignFunction
 from punchpipe.level1.deficient_pixel import DeficientPixelRemovalFunction
@@ -11,7 +17,6 @@ from punchpipe.level1.psf import PSFCorrectionFunction
 from punchpipe.level1.quartic_fit import QuarticFitFunction
 from punchpipe.level1.vignette import VignettingCorrectionFunction
 from punchpipe.level1.stray_light import StrayLightRemovalFunction
-from punchpipe.infrastructure.data import PUNCHData
 
 
 class LoadLevel0(IngestTask):
@@ -22,6 +27,53 @@ class LoadLevel0(IngestTask):
 class OutputLevel1(OutputTask):
     def write(self, data, path):
         return data.write(path)
+
+
+class Level1QueryTask(MySQLFetch):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,
+                         query="SELECT * FROM files WHERE state = 'finished' AND level = 0",
+                         **kwargs)
+
+
+class Level1InputsCheck(CheckForInputs):
+    def run(self, query_result):
+        output = []
+        date_format = "%Y%m%dT%H%M%S"
+        if query_result is not None:
+            print(query_result, type(query_result))
+            for result in query_result:
+                now = datetime.now()
+                now_time_str = datetime.strftime(now, date_format)
+                date_acquired = result[6]
+                date_obs = result[7]
+                observation_time_str = datetime.strftime(date_obs, date_format)
+                this_flow_id = f"level1_obs{observation_time_str}_run{now_time_str}"
+                new_flow = FlowEntry(
+                    flow_type="process level 1",
+                    flow_id=this_flow_id,
+                    state="queued",
+                    creation_time=now,
+                    priority=1,
+                    call_data=json.dumps({"flow_id": this_flow_id,
+                                          'input_filename': '/Users/jhughes/Desktop/repos/punchpipe/punchpipe/infrastructure/tests/L0_CL1_20211111070246.fits',
+                                          'output_filename': f'/Users/jhughes/Desktop/punchpipe_output/tests/output_{random.randint(0, 100000)}.fits'})
+                )
+                new_file = FileEntry(
+                    level=2,
+                    file_type="XX",
+                    observatory="X",
+                    file_version=1,
+                    software_version=1,
+                    date_acquired=date_acquired,
+                    date_observation=date_obs,
+                    date_end=date_obs,
+                    polarization="XX",
+                    state="queued",
+                    processing_flow=this_flow_id
+                )
+                output.append((new_flow, new_file))
+        return output
 
 
 destreak_task: ScienceTask = ScienceTask("destreak", DestreakFunction)
--- scripts/initialize.py
+++ scripts/initialize.py
@@ -6,6 +6,7 @@ from punchpipe.infrastructure.flows import LauncherFlowBuilder, SchedulerFlowBui
 from punchpipe.infrastructure.tasks.scheduler import CheckForInputs
 from punchpipe.infrastructure.db import FileEntry, FlowEntry
 from punchpipe.level1.flow import level1_core_flow
+from punchpipe.level1.tasks import Level1QueryTask, Level1InputsCheck
 from credentials import db_cred
 from prefect.storage import Local
 from prefect.tasks.mysql import MySQLFetch
@@ -18,55 +19,6 @@ if __name__ == "__main__":
     process_flows.append(level1_process_flow)
 
     scheduler_flows = []
-
-    class Level1QueryTask(MySQLFetch):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args,
-                             query="SELECT * FROM files WHERE state = 'finished' AND level = 0",
-                             **kwargs)
-
-
-    class Level1InputsCheck(CheckForInputs):
-        def run(self, query_result):
-            output = []
-            date_format = "%Y%m%dT%H%M%S"
-            if query_result is not None:
-                print(query_result, type(query_result))
-                for result in query_result:
-                    now = datetime.now()
-                    now_time_str = datetime.strftime(now, date_format)
-                    date_acquired = result[6]
-                    date_obs = result[7]
-                    observation_time_str = datetime.strftime(date_obs, date_format)
-                    this_flow_id = f"level1_obs{observation_time_str}_run{now_time_str}"
-                    new_flow = FlowEntry(
-                        flow_type="process level 1",
-                        flow_id=this_flow_id,
-                        state="queued",
-                        creation_time=now,
-                        priority=1,
-                        call_data=json.dumps({"flow_id": this_flow_id,
-                                              'input_filename': '/Users/jhughes/Desktop/repos/punchpipe/punchpipe/infrastructure/tests/L0_CL1_20211111070246.fits',
-                                              'output_filename': f'/Users/jhughes/Desktop/punchpipe_output/tests/output_{random.randint(0, 100000)}.fits'})
-                    )
-                    new_file = FileEntry(
-                        level=2,
-                        file_type="XX",
-                        observatory="X",
-                        file_version=1,
-                        software_version=1,
-                        date_acquired=date_acquired,
-                        date_observation=date_obs,
-                        date_end=date_obs,
-                        polarization="XX",
-                        state="queued",
-                        processing_flow=this_flow_id
-                    )
-                    output.append((new_flow, new_file))
-            return output
-
-
-    level1_inputs_check = Level1InputsCheck()
     level1_schedule_flow = SchedulerFlowBuilder(db_cred, 1, 1, Level1InputsCheck, Level1QueryTask).build()
     scheduler_flows.append(level1_schedule_flow)
 
