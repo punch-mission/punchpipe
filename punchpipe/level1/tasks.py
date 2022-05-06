82a2bf69c090642a67099f56b9faf54e7223271b
jmbhughes <hughes.jmb@gmail.com>
2022-05-06 01:51:26 -0600

    enabled correct file writing

--- punchpipe/level1/tasks.py
+++ punchpipe/level1/tasks.py
@@ -20,16 +20,19 @@ from punchpipe.level1.stray_light import StrayLightRemovalFunction
 
 
 class LoadLevel0(IngestTask):
+    """Loads a Level 0 for processing"""
     def open(self, path):
         return PUNCHData.from_fits(path)
 
 
 class OutputLevel1(OutputTask):
+    """Outputs a processed Level 1 file """
     def write(self, data, path):
-        return data.write(path)
+        return data.write(path + data.generate_id() + ".fits")
 
 
 class Level1QueryTask(MySQLFetch):
+    """Queries which Level 0 files are ready for the Level 0 to Level 1 processing pipeline"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args,
                          query="SELECT * FROM files WHERE state = 'finished' AND level = 0",
@@ -37,18 +40,26 @@ class Level1QueryTask(MySQLFetch):
 
 
 class Level1InputsCheck(CheckForInputs):
+    """Converts the Level1QueryTask results to a more useable format for the pipeline
+
+    Specifically, it creates a FlowEntry object and a FileEntry object for passing through the pipeline and scheduling
+    """
     def run(self, query_result):
         output = []
         date_format = "%Y%m%dT%H%M%S"
-        if query_result is not None:
-            print(query_result, type(query_result))
+        if query_result is not None:  # None can occur if we did not use "fetch all" for the query task
             for result in query_result:
+                # extract information needed for construction FlowEntry and FileEntry
                 now = datetime.now()
                 now_time_str = datetime.strftime(now, date_format)
                 date_acquired = result[6]
                 date_obs = result[7]
+                incoming_filename = result[-1]
+
                 observation_time_str = datetime.strftime(date_obs, date_format)
                 this_flow_id = f"level1_obs{observation_time_str}_run{now_time_str}"
+
+                # construct the flow entry
                 new_flow = FlowEntry(
                     flow_type="process level 1",
                     flow_id=this_flow_id,
@@ -56,9 +67,11 @@ class Level1InputsCheck(CheckForInputs):
                     creation_time=now,
                     priority=1,
                     call_data=json.dumps({"flow_id": this_flow_id,
-                                          'input_filename': '/Users/jhughes/Desktop/repos/punchpipe/punchpipe/infrastructure/tests/L0_CL1_20211111070246.fits',
-                                          'output_filename': f'/Users/jhughes/Desktop/punchpipe_output/tests/output_{random.randint(0, 100000)}.fits'})
+                                          'input_filename': incoming_filename,
+                                          'output_filename': f'/Users/jhughes/Desktop/repos/punchpipe/example_run_data'})
                 )
+
+                # construct the FileEntry
                 new_file = FileEntry(
                     level=2,
                     file_type="XX",
@@ -76,6 +89,7 @@ class Level1InputsCheck(CheckForInputs):
         return output
 
 
+# Create ScienceTasks from all the ScienceFunctions
 destreak_task: ScienceTask = ScienceTask("destreak", DestreakFunction)
 align_task: ScienceTask = ScienceTask("align", AlignFunction)
 deficient_pixel_removal_task: ScienceTask = ScienceTask("deficient_pixel_removal", DeficientPixelRemovalFunction)
@@ -86,7 +100,7 @@ quartic_fit_task: ScienceTask = ScienceTask("quartic_fit", QuarticFitFunction)
 vignetting_correction_task: ScienceTask = ScienceTask("vignetting_correction", VignettingCorrectionFunction)
 stray_light_removal_task: ScienceTask = ScienceTask("stray_light_removal", StrayLightRemovalFunction)
 
-
+# Create necessary pipeline parameters
 input_filename = Parameter("input_filename")
 output_filename = Parameter("output_filename")
 load_level0 = LoadLevel0("load_level0")
