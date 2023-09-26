27bd8dfc0677ce1a43090f54cf13c89627a69886
jmbhughes <z*5@2!lSBjO1>
2023-09-26 12:01:59 -0600

    changes version retrieval

--- punchpipe/__init__.py
+++ punchpipe/__init__.py
@@ -1,16 +1,4 @@
-# version stuff copied from https://stackoverflow.com/a/17638236
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
+# version guidance from https://stackoverflow.com/a/56331414
+from importlib.metadata import version
 
-try:
-    _dist = get_distribution('punchpipe')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'punchpipe')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install punchpipe project with setup.py'
-else:
-    __version__ = _dist.version
+__version__ = version("punchpipe")
