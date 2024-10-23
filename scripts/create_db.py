6294300d09fe943d7210842c29b19e83d53a7052
Marcus Hughes <hughes.jmb@gmail.com>
2024-10-22 22:22:38 -0600

    only create database if it doesn't exist

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 from punchpipe.controlsegment.db import Base
 from prefect_sqlalchemy import SqlAlchemyConnector
@@ -7,4 +7,6 @@ from prefect_sqlalchemy import SqlAlchemyConnector
 if __name__ == "__main__":
     credentials = SqlAlchemyConnector.load("mariadb-creds")
     engine = credentials.get_engine()
+    with engine.connect() as connection:
+        result = connection.execute(text('CREATE DATABASE IF NOT EXISTS punchpipe;'))
     Base.metadata.create_all(engine)
