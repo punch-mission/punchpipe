fac5d449f06f597bef00810f6857587acafa4695
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-11 22:26:13 -0700

    allows deploy after monitor

--- punchpipe/cli.py
+++ punchpipe/cli.py
@@ -1,4 +1,5 @@
 import subprocess
+import multiprocessing as mp
 
 import click
 from prefect import serve
@@ -15,47 +16,43 @@ from .monitor.app import create_app
 def main():
     """Run the PUNCH automated pipeline"""
 
-@main.command
-def run():
-    print("Launching punchpipe monitor on http://localhost:8050/.")
-    subprocess.Popen(["prefect", "server", "start"])
-    print("\npunchpipe Prefect flows must be stopped manually in Prefect.")
-
+def launch_monitor():
     app = create_app()
     app.run_server(debug=False, port=8051)
 
+def deploy():
     launcher_deployment = launcher_flow.to_deployment(name="launcher-deployment",
-                                                        description="Launch a pipeline segment.",
-                                                        cron="* * * * *",
-                                                        )
+                                                      description="Launch a pipeline segment.",
+                                                      cron="* * * * *",
+                                                      )
 
     level1_scheduler_deployment = level1_scheduler_flow.to_deployment(name="level1-scheduler-deployment",
-                        description="Schedule a Level 1 flow.",
-                        cron="* * * * *",
-                        )
+                                                                      description="Schedule a Level 1 flow.",
+                                                                      cron="* * * * *",
+                                                                      )
     level1_process_deployment = level1_process_flow.to_deployment(name="level1_process_flow",
                                                                   description="Process a file from Level 0 to Level 1.")
 
     level2_scheduler_deployment = level2_scheduler_flow.to_deployment(name="level2-scheduler-deployment",
-                        description="Schedule a Level 2 flow.",
-                        cron="* * * * *",
-                        )
+                                                                      description="Schedule a Level 2 flow.",
+                                                                      cron="* * * * *",
+                                                                      )
     level2_process_deployment = level2_process_flow.to_deployment(name="level2_process_flow",
                                                                   description="Process files from Level 1 to Level 2.")
 
     levelq_scheduler_deployment = levelq_scheduler_flow.to_deployment(name="levelq-scheduler-deployment",
-                        description="Schedule a Level Q flow.",
-                        cron="* * * * *",
-                        )
+                                                                      description="Schedule a Level Q flow.",
+                                                                      cron="* * * * *",
+                                                                      )
     levelq_process_deployment = levelq_process_flow.to_deployment(name="levelq_process_flow",
                                                                   description="Process files from Level 1 to Level Q.")
 
     level3_PTM_scheduler_deployment = level3_PTM_scheduler_flow.to_deployment(name="level3-PTM-scheduler-deployment",
-                        description="Schedule a Level 3 flow to make PTM.",
-                        cron="* * * * *",
-                        )
+                                                                              description="Schedule a Level 3 flow to make PTM.",
+                                                                              cron="* * * * *",
+                                                                              )
     level3_PTM_process_deployment = level3_PTM_process_flow.to_deployment(name="level3_PTM_process_flow",
-                                                                  description="Process PTM files from Level 2 to Level 3.")
+                                                                          description="Process PTM files from Level 2 to Level 3.")
 
     serve(launcher_deployment,
           level1_scheduler_deployment, level1_process_deployment,
@@ -64,3 +61,12 @@ def run():
           level3_PTM_scheduler_deployment, level3_PTM_process_deployment,
           limit=1000
           )
+
+
+@main.command
+def run():
+    print("Launching punchpipe monitor on http://localhost:8051/.")
+    subprocess.Popen(["prefect", "server", "start"])
+    print("\npunchpipe Prefect flows must be stopped manually in Prefect.")
+    mp.Process(target=launch_monitor, args=()).start()
+    deploy()
