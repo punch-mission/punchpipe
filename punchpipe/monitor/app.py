6edf93b524d58fa7ed6b2ae7d977e26adbc819c6
Sam Van Kooten <vankooten.sam@gmail.com>
2025-04-18 14:09:44 -0600

    Extrapolate the current hour's throughput

--- punchpipe/monitor/app.py
+++ punchpipe/monitor/app.py
@@ -210,26 +210,37 @@ def create_app():
         now = datetime.now()
         with get_database_session() as session:
             reference_time = now - timedelta(hours=72)
-            query = (f"SELECT flow_type, end_time, AVG(TIMEDIFF(end_time, start_time)) AS duration, COUNT(*) AS count, state "
+            query = ("SELECT flow_type, end_time AS hour, AVG(TIMEDIFF(end_time, start_time)) AS duration, "
+                     "COUNT(*) AS count, state "
                      f"FROM flows WHERE end_time > '{reference_time}' "
-                     f"GROUP BY HOUR(end_time), DAY(end_time), MONTH(end_time), YEAR(end_time), flow_type, state;")
+                     "GROUP BY HOUR(end_time), DAY(end_time), MONTH(end_time), YEAR(end_time), flow_type, state;")
             df = pd.read_sql_query(query, session.connection())
         # Fill missing entries (for hours where nothing ran)
-        df.end_time = [ts.floor('h') for ts in df.end_time]
+        df.hour = [ts.floor('h') for ts in df.hour]
         dates = pd.date_range(reference_time, now, freq=timedelta(hours=1)).floor('h')
         additions = []
         for flow_type in df.flow_type.unique():
             for date in dates:
                 for state in ['failed', 'completed']:
-                    if len(df.query(f'end_time == @date and state == @state and flow_type == @flow_type')) == 0:
+                    if len(df.query(f'hour == @date and state == @state and flow_type == @flow_type')) == 0:
                         additions.append([flow_type, date, None, 0, state])
         df = pd.concat([df, pd.DataFrame(additions, columns=df.columns)], ignore_index=True)
-        df.sort_values(['state', 'end_time'], inplace=True)
+        df.sort_values(['state', 'hour'], inplace=True)
 
-        fig_throughput = px.line(df, x='end_time', y="count", color="flow_type", title="Flow throughput", line_dash="state")
+        # Extrapolate the last hourly window
+        now_index = pd.Timestamp(now).floor('h')
+        seconds_into_hour = (now - now_index).total_seconds()
+        # But don't do it if it's really a lot of extrapolation
+        if seconds_into_hour > 120:
+            df = df.astype({"count": "float"})
+            df.loc[df['hour'] == now_index, 'count'] *= 3600 / (now - now_index).total_seconds()
+
+        fig_throughput = px.line(df, x='hour', y="count", color="flow_type", line_dash="state",
+                                 title="Flow throughput (current hour's throughput is extrapolated)")
         fig_throughput.update_xaxes(title_text="Time")
         fig_throughput.update_yaxes(title_text="Flow runs per hour")
-        fig_duration = px.line(df[df['state'] == 'completed'], x='end_time', y="duration", color="flow_type", title="Flow duration")
+        fig_duration = px.line(df[df['state'] == 'completed'], x='hour', y="duration", color="flow_type",
+                               title="Flow duration")
         fig_duration.update_xaxes(title_text="Time")
         fig_duration.update_yaxes(title_text="Average flow duration (s)")
 
