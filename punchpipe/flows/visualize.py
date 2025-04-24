953e567bd8a6da7f90a294aa71c1f91cf4a50cad
Marcus Hughes <hughes.jmb@gmail.com>
2025-04-23 21:11:38 -0600

    sort files properlly

--- punchpipe/flows/visualize.py
+++ punchpipe/flows/visualize.py
@@ -29,9 +29,10 @@ def visualize_query_ready_files(session, pipeline_config: dict, reference_time: 
                                     .filter(File.date_obs <= reference_time)
                                     .filter(File.level == level)
                                     .filter(File.file_type == product_code[0:2])
-                                    .filter(File.observatory == product_code[2]).all())
+                                    .filter(File.observatory == product_code[2])
+                                    .order_by(File.date_obs.asc()).all())
             logger.info(f"Found {len(product_ready_files)} files to make for {level}_{product_code}")
-            all_ready_files.append(sorted(list(product_ready_files)))
+            all_ready_files.append(list(product_ready_files))
             all_product_codes.append(f"L{level}_{product_code}")
 
     logger.info(f"{len(all_ready_files)} files will be used for visualization.")
