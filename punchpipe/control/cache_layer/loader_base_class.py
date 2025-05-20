e572eb365b72035ad0177cbb3f48afb00e06f240
Sam Van Kooten <vankooten.sam@gmail.com>
2025-05-20 16:20:16 -0600

    Add cache layer for L1 NFIs

--- punchpipe/control/cache_layer/loader_base_class.py
+++ punchpipe/control/cache_layer/loader_base_class.py
@@ -30,12 +30,18 @@ class LoaderABC(abc.ABC, Generic[T]):
                 result = self.load_from_disk()
                 self.try_caching(result)
             else:
-                result = pickle.loads(buffer.data)
+                result = self.from_bytes(buffer.data)
         return result, self.src_repr()
 
     def try_caching(self, object: T) -> None:
-        data = pickle.dumps(object)
+        data = self.to_bytes(object)
         manager.try_write_to_key(self.gen_key(), data)
+
+    def to_bytes(self, object: T) -> bytes:
+        return pickle.dumps(object)
+
+    def from_bytes(self, bytes: bytes) -> T:
+        return pickle.loads(bytes)
 
     def __str__(self):
         return self.__repr__()
