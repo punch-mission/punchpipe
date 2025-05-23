16c8b6405c0a3828c1e980f05b03538dc022d6db
Chris Lowder <chris.lowder@swri.org>
2025-05-23 16:57:33 -0600

    add the masks

--- scripts/add_real_calibration_to_db.py
+++ scripts/add_real_calibration_to_db.py
@@ -17,6 +17,7 @@ model_paths += sorted(glob(os.path.join(model_directory, "PUNCH_L1_FQ*.fits")))
 model_paths += sorted(glob(os.path.join(model_directory, "PUNCH_L1_G*.fits")))
 model_paths += sorted(glob(os.path.join(model_directory, "PUNCH_L1_S*.fits")))
 model_paths += sorted(glob(os.path.join(model_directory, "PUNCH_L1_D*.fits")))
+model_paths += sorted(glob(os.path.join(model_directory, "PUNCH_L1_MS*.fits")))
 
 for model_path in model_paths:
     base_path = os.path.basename(model_path)
