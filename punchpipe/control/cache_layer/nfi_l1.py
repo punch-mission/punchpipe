9a1731e12cca292b8ef01b09b3d6cdc04c9e2fa0
Sam Van Kooten <vankooten.sam@gmail.com>
2025-05-30 10:35:27 -0600

    Store image mean and median in nfi_l1 cache

--- punchpipe/control/cache_layer/nfi_l1.py
+++ punchpipe/control/cache_layer/nfi_l1.py
@@ -1,4 +1,5 @@
 import os
+import struct
 from collections.abc import Callable
 
 from ndcube import NDCube
@@ -13,15 +14,19 @@ class NFIL1Loader(LoaderABC[NDCube]):
     def __init__(self, path: str):
         self.path = path
 
-    def load(self, into: np.ndarray) -> None:
+    def load(self, into: np.ndarray) -> tuple[float, float]:
         with manager.try_read_from_key(self.gen_key()) as buffer:
             if buffer is None:
-                result = self.load_from_disk()
-                self.try_caching(result)
+                cube = self.load_from_disk()
+                mean = cube.meta['DATAAVG'].value
+                median = cube.meta['DATAMDN'].value
+                data = cube.data
+                self.try_caching((mean, median, data))
             else:
-                result = self.from_bytes(buffer.data)
-            into[:] = result
-            del result
+                mean, median, data = self.from_bytes(buffer.data)
+            into[:] = data
+            del data
+        return mean, median
 
     def gen_key(self) -> str:
         return f"nfi_l1-{os.path.basename(self.path)}-{os.path.getmtime(self.path)}"
@@ -30,13 +35,19 @@ class NFIL1Loader(LoaderABC[NDCube]):
         return self.path
 
     def load_from_disk(self) -> NDCube:
-        return load_ndcube_from_fits(self.path, include_uncertainty=False, include_provenance=False).data
+        return load_ndcube_from_fits(self.path, include_uncertainty=False, include_provenance=False)
 
-    def to_bytes(self, object: np.ndarray) -> bytes:
-        return object.tobytes()
+    def to_bytes(self, data: tuple) -> bytes:
+        mean, median, data = data
+        mean = struct.pack('f', mean)
+        median = struct.pack('f', median)
+        data_array = data.tobytes()
+        return mean + median + data_array
 
-    def from_bytes(self, bytes: bytes) -> np.ndarray:
-        return np.frombuffer(bytes, dtype=np.float64).reshape((2048, 2048))
+    def from_bytes(self, bytes: bytes) -> tuple[float, float, np.ndarray]:
+        mean = struct.unpack('f', bytes[0:4])
+        median = struct.unpack('f', bytes[4:8])
+        return mean, median, np.frombuffer(bytes[8:], dtype=np.float64).reshape((2048, 2048))
 
     def __repr__(self):
         return f"FitsFileLoader({self.path})"
