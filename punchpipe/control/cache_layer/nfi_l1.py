059960eee4d1ce7ce82ebe66a3b5c60d6b515452
Sam Van Kooten <vankooten.sam@gmail.com>
2025-06-03 14:16:33 -0600

    Cache planets/moon in each quarter

--- punchpipe/control/cache_layer/nfi_l1.py
+++ punchpipe/control/cache_layer/nfi_l1.py
@@ -5,28 +5,30 @@ from collections.abc import Callable
 from ndcube import NDCube
 import numpy as np
 from punchbowl.data import load_ndcube_from_fits
+from punchbowl.levelq.pca import find_bodies_in_image
 
 from punchpipe.control.cache_layer import manager
 from punchpipe.control.cache_layer.loader_base_class import LoaderABC
 
 
-class NFIL1Loader(LoaderABC[NDCube]):
+class NFIL1Loader(LoaderABC):
     def __init__(self, path: str):
         self.path = path
 
-    def load(self, into: np.ndarray) -> tuple[float, float]:
+    def load(self, into: np.ndarray) -> tuple[float, float, list | np.ndarray]:
         with manager.try_read_from_key(self.gen_key()) as buffer:
             if buffer is None:
                 cube = self.load_from_disk()
+                bodies_in_quarter = find_bodies_in_image(cube)
                 mean = cube.meta['DATAAVG'].value
                 median = cube.meta['DATAMDN'].value
                 data = cube.data
-                self.try_caching((mean, median, data))
+                self.try_caching((mean, median, data, bodies_in_quarter))
             else:
-                mean, median, data = self.from_bytes(buffer.data)
+                mean, median, data, bodies_in_quarter = self.from_bytes(buffer.data)
             into[:] = data
             del data
-        return mean, median
+        return mean, median, bodies_in_quarter
 
     def gen_key(self) -> str:
         return f"nfi_l1-{os.path.basename(self.path)}-{os.path.getmtime(self.path)}"
@@ -35,19 +37,23 @@ class NFIL1Loader(LoaderABC[NDCube]):
         return self.path
 
     def load_from_disk(self) -> NDCube:
-        return load_ndcube_from_fits(self.path, include_uncertainty=False, include_provenance=False)
+        cube = load_ndcube_from_fits(self.path, include_uncertainty=False, include_provenance=False)
+        return cube
 
     def to_bytes(self, data: tuple) -> bytes:
-        mean, median, data = data
+        mean, median, data, bodies_in_quarter = data
         mean = struct.pack('f', mean)
         median = struct.pack('f', median)
+        bodies = np.packbits(np.array(bodies_in_quarter)).tobytes()
         data_array = data.tobytes()
-        return mean + median + data_array
+        return mean + median + bodies + data_array
 
-    def from_bytes(self, bytes: bytes) -> tuple[float, float, np.ndarray]:
-        mean = struct.unpack('f', bytes[0:4])
-        median = struct.unpack('f', bytes[4:8])
-        return mean, median, np.frombuffer(bytes[8:], dtype=np.float64).reshape((2048, 2048))
+    def from_bytes(self, bytes: bytes) -> tuple[float, float, np.ndarray, np.ndarray]:
+        mean = struct.unpack('f', bytes[0:4])[0]
+        median = struct.unpack('f', bytes[4:8])[0]
+        packed_bodies = np.frombuffer(bytes[8:11], dtype=np.uint8)
+        bodies_in_quarter = np.unpackbits(packed_bodies, count=6*4).reshape((6, 4))
+        return mean, median, np.frombuffer(bytes[11:], dtype=np.float64).reshape((2048, 2048)), bodies_in_quarter
 
     def __repr__(self):
         return f"FitsFileLoader({self.path})"
