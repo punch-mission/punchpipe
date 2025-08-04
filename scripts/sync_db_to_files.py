81e3dbe92bc9f04e841d51481d061e94625f4f56
Sam Van Kooten <vankooten.sam@gmail.com>
2025-08-04 17:48:03 -0600

    Speedups for sync_db_to_files.py

--- scripts/sync_db_to_files.py
+++ scripts/sync_db_to_files.py
@@ -1,82 +1,107 @@
 """This is for if you're keeping a mirror of the L0 files on punch190 (to process to L1 with newer code). This script
 will add the files in the given directories to the database, skipping files that are already in the database."""
 import os
+import multiprocessing
 import sys
 from glob import glob
 from datetime import datetime
 
 from astropy.io import fits
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 from punchpipe.control.db import File
 from punchpipe.control.util import get_database_session
 
-root_dirs = sys.argv[1:]
+def read_new_file_metadata(file, path):
+    # Get the correct number of microsseconds from the FITS header
+    if file.file_type[0] in ['M', 'L']:
+        return None, None
 
-session = get_database_session()
+    with fits.open(path, disable_image_compression=True) as hdul:
+        if len(hdul) > 1 and 'DATE-OBS' in hdul[1].header:
+            p = hdul[1].header['DATE-OBS'].split('.')
+            if len(p) == 2:
+                ms = p[1]
+                ms = ms + '0' * (6 - len(ms))
+                new_dateobs = file.date_obs.replace(microsecond=int(ms))
+        if len(hdul) > 1 and 'DATE' in hdul[1].header:
+            p = hdul[1].header['DATE'].split('.')
+            if len(p) == 2:
+                ms = p[1]
+                ms = ms + '0' * (6 - len(ms))
+                new_date_created = file.date_obs.replace(microsecond=int(ms))
+    return new_dateobs, new_date_created
 
-files = set()
-for root_dir in root_dirs:
-    files.update(glob(f"{root_dir}/**/*.fits", recursive=True))
-    files.update(glob(f"{root_dir}/**/MS*/**/*.bin", recursive=True))
-    files.update(glob(f"{root_dir}/**/L*/**/*.npz", recursive=True))
+if __name__ == "__main__":
+    multiprocessing.set_start_method("forkserver")
+    root_dirs = sys.argv[1:]
 
-print(f"Found {len(files)} files on disk")
+    session = get_database_session()
 
-existing_files = session.query(File).all()
+    files = set()
+    for root_dir in root_dirs:
+        files.update(glob(f"{root_dir}/**/*.fits", recursive=True))
+        files.update(glob(f"{root_dir}/**/MS*/**/*.bin", recursive=True))
+        files.update(glob(f"{root_dir}/**/L*/**/*.npz", recursive=True))
 
-existing_files = {f.filename() for f in existing_files}
+    print(f"Found {len(files)} files on disk")
 
-print(f"Loaded {len(existing_files)} existing files from the DB")
+    existing_files = session.query(File).all()
 
-n_added = 0
-n_existing = 0
-for path in files:
-    base_path = os.path.basename(path)
-    level = base_path.split("_")[1][1]
-    code = base_path.split("_")[2][:2]
-    obs = base_path.split("_")[2][-1]
-    version = base_path.split("_")[-1].split(".")[0][1:]
-    date = datetime.strptime(base_path.split("_")[3], "%Y%m%d%H%M%S")
+    existing_files = {f.filename() for f in existing_files}
 
-    pol = 'C'
-    if code[0] in ['G', 'S', 'R', 'P', 'L']:
-        pol = code[1]
-        if pol == 'R':
-            pol = 'C'
-    if code[0] == 'X':
-        pol = 'X'
+    print(f"Loaded {len(existing_files)} existing files from the DB")
 
-    file = File(
-        level=level,
-        file_type=code,
-        observatory=obs,
-        file_version=version,
-        software_version='imported to db',
-        date_obs=date,
-        polarization=pol,
-        state='created',
-    )
+    n_added = 0
+    n_existing = 0
+    new_files = []
+    new_file_paths = []
+    print("Identifying new files...")
+    for path in tqdm(files):
+        base_path = os.path.basename(path)
+        level = base_path.split("_")[1][1]
+        code = base_path.split("_")[2][:2]
+        obs = base_path.split("_")[2][-1]
+        version = base_path.split("_")[-1].split(".")[0][1:]
+        date = datetime.strptime(base_path.split("_")[3], "%Y%m%d%H%M%S")
 
-    if file.filename() not in existing_files:
-        # Get the correct number of microsseconds from the FITS header
-        if file.file_type[0] not in ['M', 'L']:
-            with fits.open(path, disable_image_compression=True) as hdul:
-                if len(hdul) > 1 and 'DATE-OBS' in hdul[1].header:
-                    p = hdul[1].header['DATE-OBS'].split('.')
-                    if len(p) == 2:
-                        ms = p[1]
-                        ms = ms + '0' * (6 - len(ms))
-                        file.date_obs = file.date_obs.replace(microsecond=int(ms))
-                if len(hdul) > 1 and 'DATE' in hdul[1].header:
-                    p = hdul[1].header['DATE'].split('.')
-                    if len(p) == 2:
-                        ms = p[1]
-                        ms = ms + '0' * (6 - len(ms))
-                        file.date_created = file.date_obs.replace(microsecond=int(ms))
-        session.add(file)
-        n_added += 1
-    else:
-        n_existing += 1
-session.commit()
+        pol = 'C'
+        if code[0] in ['G', 'S', 'R', 'P', 'L']:
+            pol = code[1]
+            if pol == 'R':
+                pol = 'C'
+        if code[0] == 'X':
+            pol = 'X'
 
-print(f"Added {n_added} files, skipped {n_existing} files")
+        file = File(
+            level=level,
+            file_type=code,
+            observatory=obs,
+            file_version=version,
+            software_version='imported to db',
+            date_obs=date,
+            polarization=pol,
+            state='created',
+        )
+
+        if file.filename() not in existing_files:
+            new_files.append(file)
+            new_file_paths.append(path)
+            n_added += 1
+        else:
+            n_existing += 1
+
+
+    print(f"Found {n_added} new files, skipping {n_existing} existing files")
+
+    print("Reading metadata...")
+    for file, (date_obs, date_created) in zip(
+            new_files, process_map(read_new_file_metadata, new_files, new_file_paths, chunksize=10, max_workers=10)):
+        if date_obs is not None:
+            file.date_obs = date_obs
+            file.date_created = date_created
+
+    print("Adding to DB...")
+    session.bulk_save_objects(new_files)
+    session.commit()
