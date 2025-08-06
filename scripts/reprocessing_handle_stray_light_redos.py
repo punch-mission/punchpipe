703be84abc017183e41d986fff4310e71ebeab8f
Sam Van Kooten <vankooten.sam@gmail.com>
2025-08-06 17:29:30 -0600

    Fix for bootstrap phase reset script

--- scripts/reprocessing_handle_stray_light_redos.py
+++ scripts/reprocessing_handle_stray_light_redos.py
@@ -8,6 +8,7 @@ models, and all X files have a corresponding Q file. Only when all polarizer/obs
 state should the L1-late flow be enabled.
 """
 import sys
+from datetime import timedelta
 
 from punchpipe.control.db import File, Flow
 from punchpipe.control.util import get_database_session
@@ -19,47 +20,31 @@ session = get_database_session()
 did_reset = False
 for type in ['M', 'Z', 'P', 'R']:
     for observatory in ['1', '2', '3', '4']:
-        n_models = (session.query(File)
-                           .where(File.file_type == 'S' + type)
-                           .where(File.observatory == observatory)
-                           .where(File.state == 'created')
-                           .count())
-        if n_models < 2:
+        stray_light_models = (session.query(File)
+                              .where(File.file_type == 'S' + type)
+                              .where(File.observatory == observatory)
+                              .where(File.state == 'created')
+                              .all())
+        if len(stray_light_models) < 2:
             print(f"*{type}{observatory} ⏳ : waiting on stray light models")
             continue
 
-        x_files = (session.query(File)
-                          .where(File.file_type == 'X' + type)
-                          .where(File.observatory == observatory)
-                          .where(File.polarization == 'C')
-                          .where(File.level == '1')
-                          .all())
-        x_files = {f.date_obs: f for f in x_files}
-        q_files = (session.query(File)
-                          .where(File.file_type == 'Q' + type)
-                          .where(File.observatory == observatory)
-                          .where(File.polarization == 'C')
-                          .where(File.level == '1')
-                          .all())
-        q_files = {f.date_obs: f for f in q_files}
+        creation_times = [model.date_created for model in stray_light_models]
+        cutoff_time = sorted(creation_times)[1] + timedelta(minutes=1)
 
-        xs_without_qs = x_files.keys() - q_files.keys()
-        if len(xs_without_qs):
+        early_flows = (session.query(Flow)
+                       .where(Flow.creation_time < cutoff_time)
+                       .where(Flow.state != 'revivable')
+                       .all())
+
+        if len(early_flows):
             did_reset = True
-            ids_to_reset = []
-            for dateobs in xs_without_qs:
-                file = x_files[dateobs]
-                ids_to_reset.append(file.processing_flow)
-            flows_to_reset = (session.query(Flow)
-                              .where(Flow.flow_id.in_(ids_to_reset))
-                              .where(Flow.state != 'revivable')
-                              .all())
-            for flow in flows_to_reset:
+            for flow in early_flows:
                 flow.state = 'revivable'
-            print(f"*{type}{observatory} 🔄 : reset {len(flows_to_reset)} L1-early flows")
+            session.commit()
+            print(f"*{type}{observatory} 🔄 : reset {len(early_flows)} L1-early flows")
         else:
             print(f"*{type}{observatory} ✅ : looks good!")
-session.commit()
 
 if did_reset:
     print("Any running or planned stray light models may crash as we're removing their input X files, but that's OK. "
