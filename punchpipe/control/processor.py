192dac71369e404e82b1e2cd570025ca54c80a2b
Sam Van Kooten <vankooten.sam@gmail.com>
2025-10-03 17:11:13 -0600

    Make sure date_obs in the DB exactly matches what's in the file

--- punchpipe/control/processor.py
+++ punchpipe/control/processor.py
@@ -82,6 +82,7 @@ def generic_process_flow_logic(flow_id: int | list[int], core_flow_to_launch, pi
                 file_db_entry = match_data_with_file_db_entry(result, file_db_entry_list)
                 logger.info(f"Preparing to write {file_db_entry.file_id}.")
                 output_file_ids.add(file_db_entry.file_id)
+                file_db_entry.date_obs = result.meta['DATE-OBS'].value
                 result.meta['OUTLIER'] = int(file_db_entry.outlier)
                 filename = write_file(result, file_db_entry, pipeline_config)
                 logger.info(f"Wrote to {filename}")
